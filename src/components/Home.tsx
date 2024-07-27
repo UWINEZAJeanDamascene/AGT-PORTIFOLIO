9bb93a96ebf18a6dbf37bb7b75883d2ba559f920
@jay <uwinezajd2@gmail.com>
2024-07-27 17:21:48 +0300

    changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,10 +4,10 @@ const Home= () => {
   return (
     <div className="">
      <div className="w-full h-auto pb-5 flex justify-center  items-center max-sm:flex-col  bg-[#162b40]">
-         <div className='w-1/2 py-10 ml-16 animationn  '>
+         <div className='w-[88%] py-10 ml-16  animationn  '>
           <p className='text-teal-500'>Hello!</p>
           <h1 className='text-6xl font-bold'>I'm Jay-winn</h1>
-          <p className='text-yellow-600 text-3xl'>A web Front-end developer</p>
+          <p className='text-yellow-600 text-2xl'>A web Front-end developer</p>
           <p className='text-2xl py-2'>From a thousand hills</p>
           <button className='border bg-yellow-600 p-2 text-lg text-white rounded-md hover:bg-orange-300 hover:text-black font-serif font-bold'>Hire me</button>
          </div>
